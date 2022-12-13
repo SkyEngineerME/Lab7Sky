2b19d6c2afdbd84ecc012178c7091fe88baa3c94
THANATHORN TAEWATTANAPANIT <thanathorn_ta@cmu.ac.th>
2022-12-13 15:59:36 +0700

    lab7.3compleate.cpp completed

--- lab7_3.cpp
+++ lab7_3.cpp
@@ -1,15 +1,37 @@
 #include<iostream>
+#include<cmath>
 
 using namespace std;
 
+int adiff(int A,int B){
+  int C;
+  if(abs(A-B) <= 180){
+  C = abs(A-B); 
+  return C;
+  }
+  else if(abs(A-B) == 0){
+  C = 0; 
+  return C;
+  }
+  else if(abs(A-B)  > 180 && abs(A-B) < 360){
+  C = 360-abs(A-B) ;
+  return C;
+  } 
+  else if(abs(A-B) >= 360){
+  C = abs(A-B)%360;
+  return C;
+  }
+  return 0;
+}
 
 int main(){
-  cout << adiff(180,270);
-  cout << adiff(210,45);
-  cout << adiff(0,360);
-  cout << adiff(10,350);
-  cout << adiff(95,260);
-  cout << adiff(90,-90);
-  cout << adiff(1000,280);
-  cout << adiff(60,244);
+  cout << adiff(180,270) << "\n";
+  cout << adiff(210,45) << "\n";
+  cout << adiff(0,360) << "\n";
+  cout << adiff(10,350) << "\n";
+  cout << adiff(95,260) << "\n";
+  cout << adiff(90,-90) << "\n";
+  cout << adiff(1000,280) << "\n";
+  cout << adiff(60,244) << "\n";
+  return 0;
 }
